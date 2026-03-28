f6580035ca693a1827eb577d9fd2def6aea842c7
Alessandra <alessaconceicao501501@gmail.com>
2026-03-28 17:41:33 -0400

    config base system

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./modules/home/home.component')
+                .then(r => r.HomeComponent)
+    }
+];
---
+++ frontend/src/app/modules/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+
+}
