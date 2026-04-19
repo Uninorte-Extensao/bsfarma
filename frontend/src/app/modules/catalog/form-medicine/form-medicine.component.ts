fd5a2726f189ba48ff1e3798c6097e2732033ba4
AlessaSousa <alessaconceicao501501@gmail.com>
2026-04-19 15:10:13 -0400

    feat: form medicine

--- frontend/src/app/modules/catalog/form-medicine/form-medicine.component.ts
+++ frontend/src/app/modules/catalog/form-medicine/form-medicine.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-
+import { InputText } from 'primeng/inputtext';
+import { SelectModule } from "primeng/select";
+import { InputNumber } from "primeng/inputnumber";
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
+import { Button } from "primeng/button";
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-form-medicine',
-  imports: [],
+  imports: [
+    InputText,
+    SelectModule,
+    InputNumber,
+    ToggleSwitch,
+    FormsModule,
+    Button,
+    RouterLink
+],
   templateUrl: './form-medicine.component.html',
   styleUrl: './form-medicine.component.scss',
 })
 export class FormMedicineComponent {
-
+  checked: boolean = false;
 }
