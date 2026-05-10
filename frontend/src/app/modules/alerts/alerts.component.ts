08274e9d1c25633dc68c640c498ff0508ddba260
AlessaSousa <alessaconceicao501501@gmail.com>
2026-05-10 15:41:16 -0400

    feat: custom cards alert

--- frontend/src/app/modules/alerts/alerts.component.ts
+++ frontend/src/app/modules/alerts/alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-alerts',
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './alerts.component.scss',
 })
 export class AlertsComponent {
+  cards = signal<any[]>([
+    {
+      icon: 'pi pi-calendar',
+      label: 'Vencimento próximo',
+      valor: 3,
+      color: 'red'
+    },
 
+    {
+      icon: 'pi pi-home',
+      label: 'Estoque mínimo',
+      valor: 3,
+      color: 'yellow'
+    },
+
+    {
+      icon: 'pi pi-check',
+      label: 'Resolvidos hoje',
+      valor: 0,
+      color: 'green'
+    }
+  ])
 }
